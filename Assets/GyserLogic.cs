eebc191a2f2181a8afd6fcc8347db31833f3aa81
Jesse Berube <berubeje@sheridancollege.ca>
2020-01-27 22:13:30 -0500

    Edit: Gyser logic complete.

--- Assets/GyserLogic.cs
+++ Assets/GyserLogic.cs
@@ -7,20 +7,44 @@ public class GyserLogic : MonoBehaviour
 {
     public float riseSpeed = 5.0f;
     public float lifeTime = 3.0f;
-    private bool blockCollide;
+    public float damage = 5.0f;
+    public float destroySpeed = 2.0f;
 
+
+    private bool blockCollide;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveUp();
+        ReduceLifeTime();
+    }
+
+    private void ReduceLifeTime()
+    {
+        lifeTime -= Time.deltaTime;
+
+        if (lifeTime <= 0.0f)
+        {
+            DestroyGyser();
+        }
+    }
+
+    private void DestroyGyser()
+    {
+        this.gameObject.transform.localScale = new Vector3(transform.localScale.x - (destroySpeed * Time.deltaTime), transform.localScale.y, transform.localScale.z);
+       
+
+        if(transform.localScale.x <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void MoveUp()
@@ -38,6 +62,16 @@ public class GyserLogic : MonoBehaviour
         if(other.tag == "Block")
         {
             blockCollide = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        WingedSpiritController spiritCon = other.gameObject.GetComponent<WingedSpiritController>();
+
+        if (spiritCon != null)
+        {
+            spiritCon.TakeDamage(damage);
         }
     }
 }
---
+++ Assets/GyserSpawnerLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyserSpawnerLogic : MonoBehaviour
+{
+    public GameObject gyserPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 spawnPosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, transform.position.y, 0.0f);
+            
+
+            Instantiate(gyserPrefab, spawnPosition, gyserPrefab.transform.rotation);
+        }
+    }
+}

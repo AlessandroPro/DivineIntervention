322a31afe2a6d0b5b6b1a8add20cc05e6b57417a
berubeje <berubeje@sheridancollege.ca>
2020-02-13 13:39:43 -0500

    Finished writing the logic for the gyser/deathwave charge logic. Working on the ability select and visual cooldown.

--- Assets/Game/Scripts/Players/Hindrance Deity/Enemies/DeathWaveLogic.cs
+++ Assets/Game/Scripts/Players/Hindrance Deity/Enemies/DeathWaveLogic.cs
@@ -15,9 +15,6 @@ public class DeathWaveLogic : MonoBehaviour
     // Update is called once per frame
     public void SpawnDeathWave(Vector2 position, Transform wingedSpirit)
     {
-        //Vector3 spawnPosition = new Vector3(position.x, transform.position.y, 0.0f);
-        //Instantiate(gyserPrefab, spawnPosition, gyserPrefab.transform.rotation);
-
         GameObject geyser = null;
 
         for(int i = -10; i < 12; i += 2 )
@@ -26,49 +23,5 @@ public class DeathWaveLogic : MonoBehaviour
             geyser.transform.rotation = transform.rotation;
             geyser.transform.localPosition = new Vector3(i, 0, 0);
         }
-
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(-10, 0, 0);
-
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(-8, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(-6, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(-4, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(-2, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(0, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(2, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(4, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(6, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(8, 0, 0);
-
-        //geyser = Instantiate(gyserPrefab, transform);
-        //geyser.transform.rotation = transform.rotation;
-        //geyser.transform.localPosition = new Vector3(10, 0, 0);
     }
 }
--- Assets/Game/Scripts/Players/Hindrance Deity/Enemies/GyserSpawnerLogic.cs
+++ Assets/Game/Scripts/Players/Hindrance Deity/Enemies/GyserSpawnerLogic.cs
@@ -6,13 +6,40 @@ public class GyserSpawnerLogic : MonoBehaviour
 {
     public GameObject gyserPrefab;
 
+    private GyserChargeScript gyserCharge;
+
+
+    private void Start()
+    {
+        Transform child = gameObject.transform.GetChild(0);
+
+        if(child != null)
+        {
+            gyserCharge = child.GetChild(0).GetComponent<GyserChargeScript>();
+        }
+        
+    }
+
     public void SpawnGyser(Vector2 position, Transform wingedSpirit)
     {
-        //Vector3 spawnPosition = new Vector3(position.x, transform.position.y, 0.0f);
-        //Instantiate(gyserPrefab, spawnPosition, gyserPrefab.transform.rotation);
+        int verdict = gyserCharge.GyserRequest();
 
-        GameObject geyser = Instantiate(gyserPrefab, transform);
-        geyser.transform.rotation = transform.rotation;
-        geyser.transform.localPosition = new Vector3(position.x, 0, 0);
+        if(verdict == 1)
+        {
+            GameObject geyser = Instantiate(gyserPrefab, transform);
+            geyser.transform.rotation = transform.rotation;
+            geyser.transform.localPosition = new Vector3(position.x, 0, 0);
+        }
+        else if(verdict == 2)
+        {
+            GameObject geyser = null;
+
+            for (int i = -10; i < 12; i += 2)
+            {
+                geyser = Instantiate(gyserPrefab, transform);
+                geyser.transform.rotation = transform.rotation;
+                geyser.transform.localPosition = new Vector3(i, 0, 0);
+            }
+        }
     }
 }

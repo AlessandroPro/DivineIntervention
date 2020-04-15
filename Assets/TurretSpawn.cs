58f040929ce2a438436c3d200ee28f887c1d48fc
Jesse Berube <berubeje@sheridancollege.ca>
2020-04-14 21:26:25 -0400

    Starting the turretspawn effect casing

---
+++ Assets/TurretSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretSpawn : MonoBehaviour
+{
+    public MeshRenderer turretBodyRender;
+    public SphereCollider sphereCollider;
+    public MeshRenderer barrelRender;
+    public Animator fsm;
+
+
+    void Start()
+    {
+        turretBodyRender.enabled = false;
+        sphereCollider.enabled = false;
+        barrelRender.enabled = false;
+        fsm.enabled = false;
+       
+
+        Destroy(this.gameObject, 0.5f);
+    }
+
+    private void OnDestroy()
+    {
+        turretBodyRender.enabled = true;
+        sphereCollider.enabled = true;
+        barrelRender.enabled = true;
+        fsm.enabled = true;
+    }
+}
